34e0dcb266770694e9ef00268f3fddf72d24559a
Kovalenko Rodion <167234046+clownlessmode@users.noreply.github.com>
2024-12-11 17:33:33 +0700

    data in chat

--- src/entities/messages/ui/MessageCard.tsx
+++ src/entities/messages/ui/MessageCard.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { MessageEntity } from "../entities/message.entity";
 import generateColor from "@/shared/utils/generate-color";
 import Variant from "../entities/variant.enum";
+import formatDate from "@/shared/utils/format-date";
 
 interface Props {
   message: MessageEntity;
@@ -22,10 +23,15 @@ export const MessageCard = ({ message, name, id }: Props) => {
           ),
         }}
       ></div>
-      <div className="flex flex-col">
-        <h3 className="font-medium text-[16px]">
-          {message.variant === Variant.OUTGOING ? "Вы" : name}
-        </h3>
+      <div className="flex flex-col w-full">
+        <div className="justify-between flex w-full items-center">
+          <h3 className="font-medium text-[16px] justify-between flex">
+            {message.variant === Variant.OUTGOING ? "Вы" : name}
+          </h3>
+          <p className="opacity-50">
+            {formatDate(message.created_at).replace(/\.\d{2},/, "")}
+          </p>
+        </div>
         <p className="text-muted-foreground">{message.content}</p>
       </div>
     </div>
